bb01b4f26df110ec704052b691fde33921a34455
Basil Beltran <basil.beltran@gmail.com>
2025-08-05 02:55:56 -0600

    JsonLoad
    I’ve made a generic function to load from a Jason on Feil into Multiverse (nee periodic)

---
+++ server.js
@@ -0,0 +1,10 @@
+
+// Simple express server to serve static content
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
